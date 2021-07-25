7d73a5a57bdde5ac2e9d7529fd33ffdfe40b8fac
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-07-25 18:40:16 -0500

    Personalizando estilos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
 
+const Contenedor = styled.div`
+display:flex;
+align-items:center;
+padding-top:5rem;
+flex-direction: column;
+`;
+
+const Boton = styled.button`
+background: white;
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color: #000;
+border-radius: 7px;
+margin-top: 3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 2px solid black;
+`;
+  
 function App() {
+  const consultarAPI = ()=>{
+    console.log('Consultando')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener frase
+      </Boton>
+    </Contenedor>
   );
 }
 
